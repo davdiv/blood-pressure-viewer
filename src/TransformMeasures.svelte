e482a5d722cb3293fdb6fada0225bb2470f8f436
DivDE <divde@musicociel.fr>
2025-08-05 17:10:03 +0200

    Implementing filters

--- src/TransformMeasures.svelte
+++ src/TransformMeasures.svelte
@@ -1,16 +1,55 @@
 <script lang="ts">
+  import { SvelteMap } from "svelte/reactivity";
   import { computeTimeBasedAverages } from "./average";
   import type { BloodPressureMeasurement } from "./bluetooth/decoding";
+  import { applyFilters, type Filter, relevantFilters } from "./filters";
   import GraphAndTable from "./GraphAndTable.svelte";
+  import Ban from "./icons/Ban.svelte";
+  import Check from "./icons/Check.svelte";
 
   const { measures }: { measures: BloodPressureMeasurement[] } = $props();
+  const filters = $derived(relevantFilters(measures));
+  const filtersValue = new SvelteMap<Filter, boolean>();
+  const filteredMeasures = $derived(applyFilters(measures, filtersValue));
+
   let averageMode: "measure" | "day" | "week" | "all" = $state("measure");
 
-  const averages = $derived(computeTimeBasedAverages(measures));
+  const averages = $derived(computeTimeBasedAverages(filteredMeasures));
   const measuresToDisplay = $derived(
-    averageMode === "measure" ? measures : averages?.[averageMode]
+    averageMode === "measure" ? filteredMeasures : averages?.[averageMode]
   );
+
+  const toggleFilter = (filter: Filter) => {
+    const currentValue = filtersValue.get(filter);
+    if (currentValue === true) {
+      filtersValue.set(filter, false);
+    } else if (currentValue === false) {
+      filtersValue.delete(filter);
+    } else {
+      filtersValue.set(filter, true);
+    }
+  };
 </script>
+
+{#if filters.length > 0}
+  <details>
+    <summary>Filters ({filtersValue.size})</summary>
+    <div class="btn-group-vertical mb-3" role="group" aria-label="Filters">
+      {#each filters as filter}
+        {@const active = filtersValue.get(filter.filter)}
+        <button
+          title={filter.description}
+          class={["btn btn-outline-primary", { active: active != null }]}
+          onclick={() => toggleFilter(filter.filter)}
+        >
+          {#if active === true}<Check />{:else if active === false}<Ban
+            />{:else}{/if}
+          {filter.name}</button
+        >
+      {/each}
+    </div>
+  </details>
+{/if}
 
 <div class="mb-3">
   <label for="averageMode" class="form-label">Average mode</label>
@@ -22,9 +61,13 @@
   </select>
 </div>
 
-{#if measuresToDisplay}
+{#if measuresToDisplay && measuresToDisplay.length > 0}
   <GraphAndTable
     measures={measuresToDisplay}
     includeTime={averageMode === "measure"}
   />
+{:else}
+  <div class="alert alert-info">
+    There is no measure matching current filters.
+  </div>
 {/if}
---
+++ src/filters.ts
@@ -0,0 +1,78 @@
+import type { SvelteMap } from "svelte/reactivity";
+import { Status, type BloodPressureMeasurement } from "./bluetooth/decoding";
+
+export type Filter = (measure: BloodPressureMeasurement) => boolean;
+
+const isInTimeRange = (
+  timestamp: string | undefined,
+  minHour: number,
+  maxHour: number
+) => {
+  if (!timestamp) {
+    return false;
+  }
+  const hour = new Date(timestamp).getHours();
+  return hour >= minHour && hour < maxHour;
+};
+
+export const filters: { name: string; description: string; filter: Filter }[] =
+  [
+    {
+      name: "Morning",
+      description: "Morning measures (5-10 am)",
+      filter: (measure) => isInTimeRange(measure.timestamp, 5, 10),
+    },
+    {
+      name: "Evening",
+      description: "Evening measures  (6-11 pm)",
+      filter: (measure) => isInTimeRange(measure.timestamp, 18, 23),
+    },
+    {
+      name: "Irregular pulse",
+      description: "Measures with irregular pulse",
+      filter: (measure) => !!((measure.status ?? 0) & Status.IRREGULAR_PULSE),
+    },
+    {
+      name: "Missing rest",
+      description: "Measures detected as missing rest",
+      filter: (measure) => !!((measure.status ?? 0) & Status.EXT_MISSING_REST),
+    },
+    {
+      name: "x3",
+      description: "Triple measures",
+      filter: (measure) =>
+        !!((measure.status ?? 0) & Status.EXT_TRIPLE_MEASURE),
+    },
+  ];
+
+export const isFilterRelevant = (
+  filter: Filter,
+  measures: BloodPressureMeasurement[]
+) => {
+  let result = null;
+  for (const measure of measures) {
+    const newResult = !!filter(measure);
+    if (result === null) {
+      result = newResult;
+    } else if (result !== newResult) {
+      return true;
+    }
+  }
+  return false;
+};
+
+export const relevantFilters = (measures: BloodPressureMeasurement[]) =>
+  filters.filter(({ filter }) => isFilterRelevant(filter, measures));
+
+export const applyFilters = (
+  measures: BloodPressureMeasurement[],
+  filters: SvelteMap<Filter, boolean>
+) =>
+  measures.filter((measure) => {
+    for (const [filter, expectedFilterValue] of filters) {
+      if (!!filter(measure) !== expectedFilterValue) {
+        return false;
+      }
+    }
+    return true;
+  });
